0511f8367786a3aceb5a6d865b14aa78ee2f9b7e
Daniele <>
2023-08-12 11:03:58 +0200

    counterok

--- src/Components/YogaPage.js
+++ src/Components/YogaPage.js
@@ -2,6 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause,faPlus,faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
+
 import LakeImage from "../Assest/Img/lake.jpeg";
 import SeaImage from "../Assest/Img/sea.jpeg";
 import ForestImage from "../Assest/Img/forest.jpeg";
@@ -24,87 +27,56 @@ const yogaOptions = [
 function YogaPage() {
   const { optionId } = useParams();
   const selectedOption = yogaOptions.find(option => option.id === parseInt(optionId));
-  
+ 
   const [isPlaying, setIsPlaying] = useState(false);
-  const [showTimer, setShowTimer] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(selectedOption.duration);
   const audioRef = useRef(null);
-
-  const [seconds, setSeconds] = useState(selectedOption.duration);
-  const handleAudioEnded = () => {
-    setIsPlaying(false);
-    setShowTimer(false);
-    setSeconds(0);
-  };
-  // Logica del timer
-
-   useEffect(() => {
-    let timerInterval;
-
-    if (isPlaying) {
-      timerInterval = setInterval(() => {
-        setSeconds(prevSeconds => {
-          const newSeconds = prevSeconds - 1;
-          if (newSeconds <= 0) {
-            clearInterval(timerInterval);
-            audioRef.current.pause();
-            setIsPlaying(false);
-            setShowTimer(false);
-            return 0;
-          }
-          return newSeconds;
-        });
-      }, 1000);
-    } else {
-      clearInterval(timerInterval);
-    }
-
-    return () => {
-      clearInterval(timerInterval);
-    };
-  }, [isPlaying]);
-
-
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.addEventListener("ended", handleAudioEnded);
-    }
-
-    return () => {
-      if (audioRef.current) {
-        audioRef.current.removeEventListener("ended", handleAudioEnded);
-      }
-    };
-  }, []);
-
-  
 
   const handlePlayPauseClick = () => {
     if (!isPlaying) {
-      audioRef.current.currentTime = 0; // Rewind audio to the beginning
+      audioRef.current.currentTime = 0;
       audioRef.current.play();
-      setSeconds(selectedOption.duration);
-      setShowTimer(true);
+      setIsPlaying(true);
     } else {
       audioRef.current.pause();
-      setShowTimer(false);
-      setSeconds(selectedOption.duration);
+      setIsPlaying(false);
     }
-    setIsPlaying(!isPlaying);
   };
 
- 
 
-  const handleAddMinute = () => {
+  const handleAdjustTime = (amount) => {
     if (!isPlaying) {
-      setSeconds(seconds + 60);
+      setRemainingTime(prevTime => {
+        const newTime = Math.max(prevTime + amount, 0);
+        audioRef.current.currentTime = 0;
+        return newTime;
+      });
     }
   };
+  
 
-  const handleRemoveMinute = () => {
-    if (!isPlaying && seconds >= 60) {
-      setSeconds(seconds - 60);
+  useEffect(() => {
+    let interval;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setRemainingTime(prevTime => {
+          if (prevTime <= 1) {
+            audioRef.current.pause();
+            setIsPlaying(false);
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
     }
-  };
+
+    return () => clearInterval(interval);
+  }, [isPlaying]);
+
+  const formattedTime = `${Math.floor(remainingTime / 60)}:${remainingTime % 60 < 10 ? '0' : ''}${remainingTime % 60}`;
 
   const pageStyle = {
     background: `url('${selectedOption.image}') no-repeat center center fixed`,
@@ -118,32 +90,17 @@ function YogaPage() {
 
   return (
     <div style={pageStyle} className="yoga-page">
-      <div className="big-counter">
-        {Math.floor(seconds / 60)}:{seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60}
+      <h1 className='big-counter'>{formattedTime}</h1>
+      <div className='control'>
+        <button className='btn-rem' onClick={() => handleAdjustTime(-60)} disabled={isPlaying}> - </button>
+        <button className='btn-add' onClick={() => handleAdjustTime(60)} disabled={isPlaying}> + </button>
       </div>
-
-      <div className="counter">
-        <button className="btn-rem" onClick={handleRemoveMinute} disabled={isPlaying}>-</button>
-        <button className="btn-add" onClick={handleAddMinute} disabled={isPlaying}>+</button>
-      </div>
-
-      <div className="play-pause-button">
-        <button className='btn-play-pause' onClick={handlePlayPauseClick}>
-          {isPlaying ? 'Pause' : 'Play'}
-        </button>
-      </div>
-      
-      <div className="audio-player">
-        <audio ref={audioRef} src={selectedOption.music} loop></audio>
-      </div>
-     
-      <div className="home">
-        <Link to={`/`}>
-          <button className="home-button">Home</button>
-        </Link>
-      </div>
+      <button className="btn-play-pause" onClick={handlePlayPauseClick}>
+        {isPlaying ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
+      </button>
+      <audio ref={audioRef} src={selectedOption.music} loop></audio>
+      <Link to="/" className="home-button">Home</Link>
     </div>
   );
 }
-
 export default YogaPage;
