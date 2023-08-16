492daa7a4704d7b6191bdc0a3f7913f8cc51f02d
Daniele <>
2023-08-17 00:30:06 +0200

    wzx

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,53 +1,47 @@
 //---> Import di stili CSS e risorse come immagini
-import "../app.css"
-import logo from "../assets/img/g1Y8.gif"
+import "../app.css";
+import logo from "../assets/img/g1Y8.gif";
 import img from "../assets/img/img.png";
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 //---> Componente App ...
 function App() {
-
   //---> Stato per il caricamento
   const [isLoading, setIsLoading] = useState(false); // Stato per il caricamento
 
   return (
     <div className="App">
-
       {/* --->Intestazione dell'app con logo e pulsanti */}
       <header className="App-header">
         <div className="logo-container">
-
-            {/* --->Immagine logo */}
+          {/* --->Immagine logo */}
           <img src={img} className="App-img" alt="img" />
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className="button-container">
-
           {/*---> Pulsanti per le diverse opzioni di stress */}
           <Link to={`/yogapage/1`} onClick={() => setIsLoading(true)}>
             <button className="very-soft-button">Very Soft Stress</button>
           </Link>
 
-           <Link to={`/yogapage/2`} onClick={() => setIsLoading(true)}>
-          <button className="soft-button">Soft Stress</button>
+          <Link to={`/yogapage/2`} onClick={() => setIsLoading(true)}>
+            <button className="soft-button">Soft Stress</button>
           </Link>
           <Link to={`/yogapage/3`} onClick={() => setIsLoading(true)}>
-          <button className="normal-button">Normal Stress</button>
+            <button className="normal-button">Normal Stress</button>
           </Link>
           <Link to={`/yogapage/4`} onClick={() => setIsLoading(true)}>
-          <button className="hard-button">Hard Stress</button>
+            <button className="hard-button">Hard Stress</button>
           </Link>
           <Link to={`/yogapage/5`} onClick={() => setIsLoading(true)}>
-          <button className="very-hard-button ">Very Hard Stress</button>
-        </Link>
-          
+            <button className="very-hard-button ">Very Hard Stress</button>
+          </Link>
         </div>
       </header>
       <footer className="footer">
-      <p> &copy; Daniele Camodeca-®Copyright </p>
-    </footer>
-  
+        <p> &copy; Daniele Camodeca-®Copyright </p>
+      </footer>
     </div>
   );
 }
