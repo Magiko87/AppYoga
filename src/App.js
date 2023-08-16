1bd8146fd7730b40bbebc891c3a7985f784b2ad5
Daniele <>
2023-08-17 00:43:33 +0200

    poi

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 //====>Componente App
 
 //---> Import delle librerie React e componenti React Router
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import YogaPage from './components/YogaPage';
-import Loader from './components/Loader';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import YogaPage from "./components/YogaPage";
+import Loader from "./components/Loader";
 
 //---> Componente principale dell'app
 function App() {
-   //---> Stato per gestire il caricamento
+  //---> Stato per gestire il caricamento
   const [isLoading, setIsLoading] = useState(false);
 
   return (
     <div className="App">
-       {/* --->Configurazione del router */}
+      {/* --->Configurazione del router */}
       <Router>
         <Routes>
-             {/*---> Definizione delle rotte */}
+          {/*---> Definizione delle rotte */}
           <Route
             path="/"
-
             // --->Passa la funzione setIsLoading come prop al componente Home
             element={<Home setIsLoading={setIsLoading} />}
           />
           <Route path="/yogapage/:optionId" element={<YogaPage />} />
         </Routes>
       </Router>
-      
+
       {/*---> Mostra il componente Loader se isLoading è true */}
-      {isLoading && <Loader />} 
+      {isLoading && <Loader />}
     </div>
   );
 }
