708dd518934140f939052d71cee13cb93392d44b
Daniele <>
2023-08-17 00:19:31 +0200

    hg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //====>Componente App
 
-//---> Import delle librerie React e componenti React Router
+//---> Import delle librerie React e componenti React Router.
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
